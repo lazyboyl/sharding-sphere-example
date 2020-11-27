eca52c540d4b6e442a3ceaa7cfc18c78527fae33
林泽锋 <282245889@qq.com>
2020-11-27 16:55:36 +0800

    Initial commit

---
+++ src/test/java/com/sharding/mysql/example/ShardingSphereExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharding.mysql.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingSphereExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
