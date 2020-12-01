e82d16a10991662e82b5652d3275c4e5384ef48e
林泽锋 <282245889@qq.com>
2020-12-01 19:44:00 +0800

    雪花ID生成工具类的注释的修改

--- src/main/java/com/sharding/mysql/example/util/SnowflakeIdWorker.java
+++ src/main/java/com/sharding/mysql/example/util/SnowflakeIdWorker.java
@@ -4,24 +4,16 @@ package com.sharding.mysql.example.util;
  * @author linzef
  * @since 2020-11-29
  * 类描述： 生成雪花ID的工具类
- * Twitter_Snowflake<br>
- * SnowFlake的结构如下(每部分用-分开):<br>
- * 0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
- * 1位标识，由于long基本类型在Java中是带符号的，最高位是符号位，正数是0，负数是1，所以id一般是正数，最高位是0
- * 41位时间截(毫秒级)，注意，41位时间截不是存储当前时间的时间截，而是存储时间截的差值（当前时间截 - 开始时间截)
- * 得到的值），这里的的开始时间截，一般是我们的id生成器开始使用的时间，由我们程序来指定的（如下下面程序IdWorker类的startTime属性）。41位的时间截，可以使用69年，年T = (1L << 41) / (1000L * 60 * 60 * 24 * 365) = 69
- * 10位的数据机器位，可以部署在1024个节点，包括5位datacenterId和5位workerId
- * 12位序列，毫秒内的计数，12位的计数顺序号支持每个节点每毫秒(同一机器，同一时间截)产生4096个ID序号
- * 加起来刚好64位，为一个Long型。
- * SnowFlake的优点是，整体上按照时间自增排序，并且整个分布式系统内不会产生ID碰撞(由数据中心ID和机器ID作区分)，并且效率较高，经测试，SnowFlake每秒能够产生26万ID左右。
  */
 public class SnowflakeIdWorker {
 
     /**
      * 构造函数
      *
-     * @param workerId     工作ID (0~31)
-     * @param dataCenterId 数据中心ID (0~31)
+     * @param modCoefficient 当前取余的系数，例如你设置为32，则生成的id % 32 则会等于你当前设置的modVal
+     * @param modVal         当前生成的需要取余的值,假设你希望生成的雪花ID需要取余为8，则设置为8，这个值必须要小于31,且要小于modCoefficient的值
+     * @param workerId       工作ID (0~31)
+     * @param dataCenterId   数据中心ID (0~31)
      */
     public SnowflakeIdWorker(int modCoefficient, int modVal, long twepoch, long workerId, long dataCenterId) {
         this.modCoefficient = modCoefficient;
@@ -51,7 +43,7 @@ public class SnowflakeIdWorker {
     private final int modVal;
 
     /**
-     * 开始时间截 (2015-01-01)
+     * 开始时间截
      */
     private final long twepoch;
 
@@ -91,17 +83,17 @@ public class SnowflakeIdWorker {
     private final long workerIdShift = 12L;
 
     /**
-     * 数据标识id向左移17位(12+5)
+     * 数据标识id向左移17位(7+5+5)
      */
     private final long dataCenterIdShift = sequenceBits + workerIdBits + modBits;
 
     /**
-     * 时间截向左移22位(5+5+12)
+     * 时间截向左移22位(7+5+5+5)
      */
     private final long timestampLeftShift = sequenceBits + workerIdBits + dataCenterIdBits + modBits;
 
     /**
-     * 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095)
+     * 生成序列的掩码，这里为128（2^7）
      */
     private final long sequenceMask = -1L ^ (-1L << sequenceBits);
 
@@ -116,7 +108,7 @@ public class SnowflakeIdWorker {
     private long dataCenterId;
 
     /**
-     * 毫秒内序列(0~4095)
+     * 毫秒内序列(0~128)
      */
     private long sequence = 0L;
 
@@ -136,8 +128,7 @@ public class SnowflakeIdWorker {
 
         //如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
         if (timestamp < lastTimestamp) {
-            throw new RuntimeException(
-                    String.format("Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
+            throw new RuntimeException(String.format("时钟异常，请重试生成雪花ID", lastTimestamp - timestamp));
         }
 
         //如果是同一时间生成的，则进行毫秒内序列
@@ -192,7 +183,10 @@ public class SnowflakeIdWorker {
      * 测试
      */
     public static void main(String[] args) {
-        SnowflakeIdWorker idWorker = new SnowflakeIdWorker(16, 122, 1606660865000L, 0, 0);
+        /**
+         * 取余的系数设置为16，取余的值设置为13，这样你会发现生成的雪花ID的值被16取余以后为13
+         */
+        SnowflakeIdWorker idWorker = new SnowflakeIdWorker(16, 13, 28778794000L, 0, 0);
         for (int i = 0; i < 100; i++) {
             long id = idWorker.nextId();
             System.out.println((Long.toBinaryString(id)));
